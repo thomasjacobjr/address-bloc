874708aab34aa477b5af3edab8e6a249b47996e1
thomasjacobjr <thomasjacobjr@gmail.com>
2015-11-18 16:49:42 -0700

    Checkpoint 23 completed, menu continued

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -80,7 +80,65 @@ class MenuController
   end
 
   def search_entries
+    print "Search by name: "
+    name = gets.chomp
+    match = @address_book.binary_search(name)
+    system "clear"
+    if matchputs match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
   end
+
+  def search_submenu(entry)
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+
+    selection = gets.chomp
+
+    case selection
+     when "d"
+       system "clear"
+       delete_entry(entry)
+       main_menu
+     when "e"
+       edit_entry(entry)
+       system "clear"
+       main_menu
+     when "m"
+       system "clear"
+       main_menu
+     else
+       system "clear"
+       puts "#{selection} is not a valid input"
+       puts entry.to_s
+       search_submenu(entry)
+     end
+   end
+
+  def delete_entry
+    @address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def edit_entry(entry)
+     print "Updated name: "
+     name = gets.chomp
+     print "Updated phone number: "
+     phone_number = gets.chomp
+     print "Updated email: "
+     email = gets.chomp
+
+     entry.name = name if !name.empty?
+     entry.phone_number = phone_number if !phone_number.empty?
+     entry.email = email if !email.empty?
+     system "clear"
+
+     puts "Updated entry:"
+     puts entry
+   end
 
   def read_csv
     print "Enter CSV file to import: "
@@ -115,7 +173,10 @@ class MenuController
           when "n"
 
           when "d"
+            delete_entry(entry)
           when "e"
+            edit_entry(entry)
+            entry_submenu(entry)
 
           when "m"
             system "clear"
