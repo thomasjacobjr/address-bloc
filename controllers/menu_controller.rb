b453089e07d1760b86e87998a831a201a4ca97df
thomasjacobjr <thomasjacobjr@gmail.com>
2015-11-18 16:44:33 -0700

    Checkpoint 23 complete, menu continued

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -2,13 +2,13 @@ require_relative '../models/address_book'
 
 class MenuController
   attr_accessor :address_book
-  
+
   def initialize
     @address_book = AddressBook.new
   end
-  
+
   def main_menu
-  
+
     puts "Main Menu - #{@address_book.entries.count} entries"
     puts "1 - View all entries"
     puts "2 - Create an entry"
@@ -16,10 +16,10 @@ class MenuController
     puts "4 - Import entries from a CSV"
     puts "5 - Exit"
     print "Enter your selection: "
-    
+
     selection = gets.to_i
     puts "You picked #{selection}"
-    
+
     case selection
     when 1
       system "clear"
@@ -39,68 +39,84 @@ class MenuController
       main_menu
     when 5
       puts "Good-bye!"
-      
+
       exit(0)
-      
+
     else
       system "clear"
       puts "Sorry, that is not a valid input"
       main_menu
     end
   end
-  
+
   def view_all_entries
-      
+
       @address_book.entries.each do |entry|
       system "clear"
       puts entry.to_s
-      
+
       entry_submenu(entry)
       end
-      
+
       system "clear"
       puts "End of entries"
   end
-  
+
   def create_entry
       system "clear"
       puts "New AddressBloc Entry"
-      
+
       print "Name: "
       name = gets.chomp
       print "Phone number: "
       phone = gets.chomp
       print "Email: "
       email = gets.chomp
-      
+
       @address_book.add_entry(name, phone, email)
-      
+
       system "clear"
       puts "New entry created"
   end
-  
+
   def search_entries
   end
-  
+
   def read_csv
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+      entry_count = @address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is no a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
   end
-  
+
   def entry_submenu(entry)
-      
+
       puts "n - next entry"
       puts "d - delete entry"
       puts "e - edit this entry"
       puts "m - return to main menu"
-      
+
       selection = gets.chomp
-      
+
       case selection
-          
+
           when "n"
-          
+
           when "d"
           when "e"
-          
+
           when "m"
             system "clear"
             main_menu
@@ -111,4 +127,3 @@ class MenuController
       end
   end
 end
-
